39674e03585ed26a4b5f630f2d0c14c0bb0be24c
Samuel Wilder <pebazium@gmail.com>
2018-08-04 10:49:31 -0400

    Added recursive index function

--- src/main.py
+++ src/main.py
@@ -1,5 +1,27 @@
 import yaml, pprint
 
+
+def wing_recursive_get(dictn, keys):
+	"""
+	Recursively indexes a dictionary to retrieve a value.
+
+	Equivalent to:
+	dictn[keys[0]][keys[1]][keys[2]][keys[n]]
+
+	Args:
+		dictn(dict): the dictionary to index.
+		keys(list): the list of keys to index with.
+
+	Returns:
+		The value of the very last nested dict in the base dict.
+	"""
+	if len(keys) == 1:
+		return dictn[keys[0]]
+	else:
+		return get(dictn[keys[0]], keys[1:])
+
+
+
 # When importing, only load ast['Program']
 # When running, first run ast['Program'], then run ast['Main']
 
@@ -21,14 +43,32 @@ def query_symbol_table(name, ast, scope):
 
 
 
-def handle_function(line):
+def handle_function(line, ast, keys):
+	print(ast)
+	#print(line)
 	operation = getkey(line)
 
 	if operation == 'set':
-		#wing_set(getvalue(line))
-		if isinstance(getvalue(line), dict):
+
+		value = getvalue(line)
+
+		# Expressions are dictionaries
+		if isinstance(value, dict):
 			result = handle_op(getvalue(line))
 			print(result)
+
+		# Should
+		elif isinstance(value, list):
+			if len(value) == 2:
+				name = value[0]
+
+				# For right now, we are assuming that there is no expression
+				# (dict) here:
+				set_to_value = value[1]
+
+				ast = [{ name : set_to_value }] + ast # <-------------- doesn't change it because ast is local copy
+
+
 
 
 with open('test/test.yaml') as file:
@@ -44,7 +84,7 @@ with open('test/test.yaml') as file:
 		if not isinstance(line, dict):
 			raise Exception('Line is not dictionary: %s' % repr(line))
 
-		handle_function(line)
+		handle_function(line, ast['Program'], [])
 
 		
 
