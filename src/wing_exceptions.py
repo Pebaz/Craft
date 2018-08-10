2b5969888a66c3c3600eff04cd4c3bc8e75fd12a
Samuel Wilder <pebazium@gmail.com>
2018-08-10 19:57:17 -0400

    Back to a single file

--- src/wing_exceptions.py
+++
@@ -1,9 +0,0 @@
-class WingFunctionReturnException(Exception):
-	"""
-	For returning values from functions. The `wing_call` function will catch
-	these exceptions and return the value contained in this class as the return
-	value.
-	"""
-	def __init__(self, value):
-		Exception.__init__(self)
-		self.return_value = value
