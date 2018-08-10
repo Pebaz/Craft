6a229d4025e479bfbdb7b7e289e17297f10a7219
Samuel Wilder <pebazium@gmail.com>
2018-08-09 20:55:08 -0400

    Found solution that may work

--- src/syntax_test.py
+++ src/syntax_test.py
@@ -1,4 +1,5 @@
 from pyparsing import *
+from pprint import *
 
 Identifier = Word(alphanums + '!#$%&()*+,./;<=>?@\\^-_`{|}~')
 Value = (QuotedString('"') | Identifier)
@@ -8,19 +9,60 @@ Function = Forward()
 List = Forward()
 
 Function << Dict(Group(
-	(Identifier + Literal(':')) +
-	LBRACKET +
-	ZeroOrMore(Function | List | Value) +
-	RBRACKET
+	Identifier +
+	Literal(':') +
+	Group(
+		LBRACKET +
+		ZeroOrMore(Function | List | Value) +
+		RBRACKET
+	)
 ))
 
 List << Group(
 	LBRACKET +
-	ZeroOrMore(Value) +
+	ZeroOrMore(Value | List) +
 	RBRACKET
 )
 
-print(Function.parseString(open('test/syntax_test.txt').read()))
+results = Function.parseString(open('test/syntax_test.txt').read())
+
+pp = PrettyPrinter(width=1)
+#pp.pprint(results.asDict())
+
+print(results[0])
 
 
+{
+	'Program':
+	{
+		'print': [],
+		'struct': ['point', 'x', 'y'],
+		'def': {'set': {'+': ['$the-point.x', '4']}},
+		'set': {'new': ['$point', '0', '0']},
+		'move-point': {'byval': ['$a']},
+		'show-point': {'byval': ['$a']}
+	}
+}
 
+['Program', ':',
+	[
+		['print', ':', []],
+		['struct', ':', ['point', 'x', 'y']],
+		['def', ':',
+			[
+				['show-point', 'the-point'],
+				['print', ':', ['$the-point.x', '$the-point.y']]
+			]
+		],
+		['def', ':',
+			[
+				['move-point', 'the-point'],
+				['set', ':', ['$the-point.x', ['+', ':', ['$the-point.x', '4']]]]
+			]
+		],
+		['set', ':', ['a', ['new', ':', ['$point', '0', '0']]]],
+		['move-point', ':', [['byval', ':', ['$a']]]],
+		['move-point', ':', [['byval', ':', ['$a']]]],
+		['show-point', ':', [['byval', ':', ['$a']]]]
+	]
+]
