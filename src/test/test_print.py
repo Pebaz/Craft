2b77b66e25664d99399073dbdf90057c5b84a701
Samuel Wilder <pebazium@gmail.com>
2018-10-02 22:20:11 -0400

    Added more tests for the print function

--- src/test/test_print.py
+++ src/test/test_print.py
@@ -20,5 +20,36 @@ def test_print_str():
 	assert(capture_stdout(parse_source(source)) == 'Hello World!\n')
 
 
+def test_print_int():
+	"""
+	Test to see if the print function will print an int value.
+	"""
+	source = \
+	"""
+	Program:
+	[
+		print: [123]
+	]
+	"""
+	assert(capture_stdout(parse_source(source)) == '123\n')
+
+
+def test_print_float():
+	"""
+	Test to see if the print function will print a float value.
+	"""
+	source = \
+	"""
+	Program:
+	[
+		print: [3.14]
+	]
+	"""
+	assert(capture_stdout(parse_source(source)) == '3.14\n')
+
+
 if __name__ == '__main__':
 	test_print_str()
+	test_print_int()
+	test_print_float()
+	
