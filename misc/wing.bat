003aacffb53ff341a51e1bde6999d84406dd0579
Samuel Wilder <pebazium@gmail.com>
2019-02-26 13:35:43 -0500

    Removed extraneous wing.bat's

--- misc/wing.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Wing
-pushd .
-cd ../src
-python wing.py %*
-popd
-@echo on
--- misc/wing2.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title Wing
-SET file=%~dpnx1
-pushd .
-cd ../src
-python wing.py %file%
-popd
-@echo on
