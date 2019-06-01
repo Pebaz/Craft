fc7c3f350cee37c8ab713be987fdcd8bec50e028
Pebaz <pebazium@gmail.com>
2019-05-31 22:29:24 -0400

    Save

--- dev/test_new_jit/hello.c
+++ dev/test_new_jit/hello.c
@@ -4,10 +4,32 @@
 
 #define PY_NONE Py_BuildValue("")
 
+static void print_str(PyObject *o)
+{
+    PyObject_Print(o, stdout, Py_PRINT_RAW);
+}
+
+static void print_repr(PyObject *o)
+{
+    PyObject_Print(o, stdout, 0);
+}
+
 PyObject * craft_main()
 {
-    printf("Here\n");
-	return PY_NONE;
+    printf("Zero.\n");
+    //PyGILState_STATE gstate;
+    //gstate = PyGILState_Ensure();
+
+    PyObject * name = Py_BuildValue("s", "Pebaz");
+    Py_INCREF(name);
+    pyprint(name);
+    printf("-> %i\n", square(20));
+    printf("One.\n");
+    //Py_DECREF(name);
+
+    PyObject * None = PY_NONE;
+    //PyGILState_Release(gstate);
+	return None;
 }
 
 
--- dev/test_new_jit/jit_compile.py
+++ dev/test_new_jit/jit_compile.py
@@ -1,16 +1,29 @@
 import ctypes
 from pytcc import TCC
 
+def myprint(arg):
+    print(arg)
+
+def square(n):
+    return n ** 2
+
 comp = TCC()
+comp.preprocessor_symbols["DEBUG"] = "1"
 comp.add_include_path('C:/Python37/include')
 comp.add_library_path('./')
 comp.add_library_path('C:/Python37')
 comp.add_library('python37')
 # comp.add_file("hello.c")
 # comp.compile_string('')
+squaresignature = ctypes.CFUNCTYPE(ctypes.c_int, ctypes.c_int)
+func = squaresignature(square)
+pyprint = ctypes.CFUNCTYPE(ctypes.c_voidp, ctypes.c_voidp)
+comp.add_symbol("pyprint", pyprint(myprint))
+comp.add_symbol("square", func)
 comp.compile_file('hello.c')
 comp.relocate()
 
+print("Running JIT Compiled Code")
 craft_main_proto = ctypes.CFUNCTYPE(ctypes.py_object)
 craft_main = craft_main_proto(comp.get_symbol('craft_main'))
 print(craft_main())
